92004a7a9a467cb9b260807afbda05c9b8100e02
David Caballero <davidcaballeroe@gmail.com>
2020-04-28 22:12:15 -0500

    Added test case for recurseBuildKDTree, all tests passed

--- orthogonalRangeSearch.py
+++ orthogonalRangeSearch.py
@@ -1,5 +1,5 @@
 import unittest
-from avl import TreeNode, AVL_Tree
+
 
 
 from typing import List, Tuple
@@ -30,7 +30,7 @@ class OrthogonalRangeSearch():
 		if len(points) == 0: 
 			return None
 		if len(points) == 1:
-			return TreeNode(point[0])
+			return TreeNode(points[0])
 
 		# 2 Cases, on even depth split points with a vertical lines, on odd depth split with a horizonal line
 		if depth % 2 == 0:
@@ -38,7 +38,7 @@ class OrthogonalRangeSearch():
 			# p1 will be points to the left or on l, while p2 will be points to the right of l
 			
 			points = sorted(points) #TODO: Implement faster version of this
-			median = self.getMedian(sortedPoints)
+			median = self.getMedian(points, 0)
 			p1 = [point for point in points if point[0] <= median]
 			p2 = [point for point in points if point[0] > median]
 		
@@ -47,14 +47,14 @@ class OrthogonalRangeSearch():
 			# p1 will be points to the below or on l, while p2 will be points to the above of l
 			
 			points = sorted(points, key = lambda x: x[1]) #TODO: Implement faster version of this
-			median = self.getMedian(sortedPoints)
+			median = self.getMedian(points, 1)
 
 			p1 = [point for point in points if point[1] <= median]
 			p2 = [point for point in points if point[1] > median]
 
 		# Recursively call function to create subtrees
-		leftChild = resurseBuildKDTree(p1, depth + 1)
-		rightChild = recurseBuildKDTree(p2, depth + 1)
+		leftChild = self.recurseBuildKDTree(p1, depth + 1)
+		rightChild = self.recurseBuildKDTree(p2, depth + 1)
 
 		# Create node representing the separating line
 		newNode = TreeNode(median)
@@ -64,13 +64,13 @@ class OrthogonalRangeSearch():
 		return newNode
 
 
-	# recurseSearchKDTree --------------------------------------------------------------------------------------------
+	# recurseSearchKDTree ------------------------------------------------------------------------------------------
 	# A recursive algorithm that searches a kdTree for points that fall in the region specified
-	#
 	# Modifies --> None
 	# Returns  --> set of points from the kdTree in region
+	
 	def searchKDTree(self, kdTree : TreeNode, region):
-
+		print("TODO")
 
 
 
@@ -86,13 +86,15 @@ class OrthogonalRangeSearch():
 
 
 	# getMedian --------------------------------------------------------------------------------------------
-	# Simply returns the median value of a given list of points assuming that they are sorted
-	# Input --> List[Points]
+	# Returns the median value of a given list of points assuming that they are sorted, takes a flag representing if the x or y value should be used
+	# a 0 represents x and 1 represents y
+	# Input --> List[Points], int
 	#
 	# Modifies --> None
 	# Returns  --> float
-	def getMedian(self, points) -> float:
-		return ( points[int(len(points) / 2)] + points[int((len(points) -1) / 2)] ) / 2
+
+	def getMedian(self, points, x) -> float:
+		return ( points[int(len(points) / 2)][x] + points[int((len(points) -1) / 2)][x] ) / 2
 
 
 class TestOrthogonalRangeSearch(unittest.TestCase):
@@ -105,9 +107,26 @@ class TestOrthogonalRangeSearch(unittest.TestCase):
 
     def test_getMedian(self):
     	ORS = OrthogonalRangeSearch()
-    	self.assertEqual(ORS.getMedian([1,2,3,4,5,6,7]) , 4, "getMedian() Failed")
-    	self.assertEqual(ORS.getMedian([1,2,3,4,5,5,6,7]) , 4.5, "getMedian() Failed")
-    	self.assertEqual(ORS.getMedian([1,2]) , 1.5, "getMedian() Failed")
+    	self.assertEqual(ORS.getMedian( [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7)] , 0) , 4, "getMedian() Failed")
+    	self.assertEqual(ORS.getMedian( [(1,1),(2,2),(3,3),(4,4),(5,5),(5,5),(6,6),(7,7)], 1) , 4.5, "getMedian() Failed")
+    
+
+
+    def test_recurseBuildKDTree(self):
+
+    	# Takes the root node of a tree and modifies the result List to represent an in order traversal of the tree
+    	def inOrderTraversal(root: TreeNode, result: List):
+    		if root:
+    			inOrderTraversal(root.left, result)
+    			result.append(root.val)
+    			inOrderTraversal(root.right, result)
+
+    	ORS = OrthogonalRangeSearch()
+    	testPoints = [(1,1),(2,4),(5,3),(8,7),(9,10),(40,30)]
+    	testTree = ORS.recurseBuildKDTree(testPoints, 0)
+    	inOrder = []
+    	inOrderTraversal(testTree, inOrder)
+    	self.assertEqual(inOrder, [(1,1), 3.0, (5,3), 3.0, (2,4), 6.5, (8,7), 8.5, (9,10), 10.0, (40,30)], "recurseBuildKDTree() Failed")
 
 
 
